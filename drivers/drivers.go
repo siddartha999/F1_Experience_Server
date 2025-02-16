37690562c0413fcf3808a62cc9b440509f8ca8bd
Siddartha Tummala <sidddddd@amazon.com>
2025-02-16 15:03:03 -0800

    Introduced functionality to parse driver's cache

--- drivers/drivers.go
+++ drivers/drivers.go
@@ -1,6 +1,7 @@
 package drivers
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -9,7 +10,21 @@ import (
 	constants "github.com/siddartha999/F1_Experience_Server/config"
 )
 
-func getDriversFromRemote() {
+type Driver struct {
+	DriverNumber  uint8  `json:"driver_number"`
+	BroadCastName string `json:"broadcast_name"`
+	FullName      string `json:"full_name"`
+	NameAcronym   string `json:"name_acronym"`
+	TeamName      string `json:"team_name"`
+	TeamColor     string `json:"team_colour"`
+	FirstName     string `json:"first_name"`
+	LastName      string `json:"last_name"`
+	HeadShotURL   string `json:"headshot_url"`
+	CountryCode   string `json:"country_code"`
+}
+
+// Fetches F1 driver's info from a remote server
+func getDriversInfoFromRemote() {
 	cacheFile, err := os.Create(constants.DriversCacheFile)
 	if err != nil {
 		fmt.Println("Unable to Create driver's cache file")
@@ -33,10 +48,41 @@ func getDriversFromRemote() {
 	}
 }
 
+// Parses the cached driver's information
+func parseDriversCache() {
+	driversCacheBytes, err := os.ReadFile(constants.DriversCacheFile)
+	if err != nil {
+		fmt.Println("Error parsing driver's cache file")
+	}
+
+	allDrivers := []Driver{}
+	err = json.Unmarshal(driversCacheBytes, &allDrivers)
+	if err != nil {
+		fmt.Println("Error parsing driver's cache bytes", err)
+	}
+
+	//Extract unique drivers
+	uniqueDriverEntries := []Driver{}
+	isDriverEntryUnique := map[string]bool{}
+	for idx := 0; idx < len(allDrivers); idx++ {
+		currentDriverEntry := allDrivers[idx]
+		uniqueIdentifier := string(currentDriverEntry.DriverNumber) + ":" + currentDriverEntry.NameAcronym + ":" + currentDriverEntry.FullName
+		exists := isDriverEntryUnique[uniqueIdentifier]
+		if !exists {
+			uniqueDriverEntries = append(uniqueDriverEntries, currentDriverEntry)
+			isDriverEntryUnique[uniqueIdentifier] = true
+		}
+	}
+	fmt.Printf("Found %d unique driver entries", len(uniqueDriverEntries))
+	fmt.Println(uniqueDriverEntries)
+}
+
 func InitiateDriversInfo() {
 	driversCacheFileSize, err := os.Stat(constants.DriversCacheFile)
 	if err != nil || driversCacheFileSize.Size() == 0 {
 		fmt.Println("Driver's cache file does not exist on the server. Retrieving from remote")
-		getDriversFromRemote()
+		getDriversInfoFromRemote()
 	}
+	fmt.Println("Parsing cached driver's info")
+	parseDriversCache()
 }
